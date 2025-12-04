6fb111c29ceae7c918ad023bb509e75b744058b3
Fatima Asadie <adtfatyma3@gmail.com>
2025-12-04 22:20:11 +0330

    style: set up form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
 import { useState } from "react";
 import { userSchema, type TuserSchema } from "./schemas/users";
-import { useForm } from "react-hook-form";
+import { Form, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/components/ui/button"
 
+import {
+  Field,
+  FieldContent,
+  FieldDescription,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+  FieldLegend,
+  FieldSeparator,
+  FieldSet,
+  FieldTitle,
+} from "@/components/ui/field"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
+
+import { Input } from "./components/ui/input";
 function App() {
   const form = useForm<TuserSchema>({
     defaultValues: {
@@ -20,11 +43,40 @@ function App() {
     console.log(data);
   });
 
-  const [step, setStep] = useState<number>(1);
+  const [step, setStep] = useState<number>(0);
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen border">
-      <Button className="">submit</Button>
+
+    <div className="h-screen flex justify-center items-center">
+      
+            <Card  className="w-full max-w-sm">
+  <CardHeader>
+    <CardTitle>Form</CardTitle>
+  </CardHeader>
+  <CardContent>
+    <FieldSet>
+
+  <FieldGroup>
+    <Field>
+      <FieldLabel htmlFor="name">Full name</FieldLabel>
+      <Input placeholder="please enter your name" id="name" />
+      
+    </Field>
+    <Field>
+      <FieldLabel htmlFor="email">Emial</FieldLabel>
+      <Input id="email" placeholder="please enter your email" aria-invalid />
+     
+    </Field>
+    <Field orientation="">
+      <FieldLabel htmlFor="newsletter">Subscribe to the newsletter</FieldLabel>
+    </Field>
+  </FieldGroup>
+</FieldSet>
+  </CardContent>
+  <CardFooter>
+  </CardFooter>
+</Card>
+    
     </div>
   );
 }
